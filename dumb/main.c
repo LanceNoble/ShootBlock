815fe3258d11eadcc90ad9d64920bfdc6c4d4add
LanceNoble <ltnlikespie@gmail.com>
2024-09-29 20:21:17 -0400

    add macros for constructing and deconstructing messages between server and client

--- dumb/main.c
+++ dumb/main.c
@@ -1,30 +1,30 @@
-/*
-cli tcp msg (5 bytes):
-bi0     = type
-bi1-6   = bodyLength
-bi7-38  = body
-bi39-40 = padding
-
-join:
-bi0     = 0
-bi1-6   = 100000
-bi7-38  = ip
-bi39-40 = 00
-
-leave:
-bi0     = 1
-bi1-6   = 000100
-bi7-38  = 000...id
-bi39-40 = 00
-
-convert msg to bin:
-1. 
-*/
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
 
-#pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "Ws2_32.lib") 
+
+#define MAX_PLAYERS 4
+#define STRIP(by, mask, start) (by & 0b##mask) >> start
+// TODO:
+// Check for max player count
+// TODO: 
+// our client-server's TCP connections are temporary
+// so the server won't know if the client disconnects by force closing the program instead of pressing the leave button
+// we could check if the client hasn't sent a message in a while, but they could just be afk
+// therefore, implement a heartbeat system where the client must send a udp message indicating they're alive every fixed interval
+// TODO: 
+// allocate bits specifying the message's intended length so client knows if the whole message was sent or not
+// Construct a message responding to a client's TCP request
+// bit0-0 - the client's request
+//		0 - request to join the server
+//		1 - request to leave the server
+// bit1-3 - the client's id
+//		0 - request fail
+// bit4-7 - padding
+#define BUILD_TCP(type, id) type | (id << 1);
 
 u_long mode = 1;
 SOCKET hostTCP;
@@ -33,28 +33,8 @@ struct addrinfo* addrTCP;
 struct addrinfo* addrUDP;
 
 struct node {
-	char* ip;
-	struct node* next;
+	int hi;
 };
-void dtob(char c, char b[9]) {
-	int i = 0;
-	int j = 7;
-	for (int i = 0; i < 8; i++) b[i] = '0';
-	while (c != 0) {
-		if (c % 2 == 0) b[i] = '0';
-		else b[i] = '1';
-		c /= 2;
-		i++;
-	}
-	i = 0;
-	while (j > i) {
-		b[j] = b[j] ^ b[i];
-		b[i] = b[j] ^ b[i];
-		b[j] = b[j] ^ b[i];
-		i++;
-		j--;
-	}
-}
 void hsock(int type, int proto, struct addrinfo* addr, SOCKET* sock, int pipe[5]) {
 	struct addrinfo hints;
 	ZeroMemory(&hints, sizeof(hints));
@@ -71,68 +51,75 @@ void hsock(int type, int proto, struct addrinfo* addr, SOCKET* sock, int pipe[5]
 	pipe[3] = WSAGetLastError();
 }
 int main() {
-	for (int i = 0; i < 32; i++) {
-		char b[9];
-		b[8] = '\0';
-		dtob(i, b);
-		printf("%i = %s\n", i, b);
-	}
-	exit(1);
-	char** clis = calloc(2048, sizeof(char*));
-	if (clis == NULL) {
-		printf("malloc fail\n");
-		return 1;
-	}
 	int pipeTCP[4];
 	int pipeUDP[4];
 	WSADATA wsaData;
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) goto end;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) return 1;
+
 	hsock(SOCK_STREAM, IPPROTO_TCP, addrTCP, &hostTCP, pipeTCP);
 	hsock(SOCK_DGRAM, IPPROTO_UDP, addrUDP, &hostUDP, pipeUDP);
 	for (int i = 0; i < 4; i++) if (pipeTCP[i] != 0 || pipeUDP[i] != 0) goto end;
 	listen(hostTCP, SOMAXCONN);
+
 	fd_set reads;
 	FD_ZERO(&reads);
 	const struct timeval timeout = { .tv_sec = 0, .tv_usec = 0 * 1000000 };
-	struct sockaddr_in newGuest;
-	int numCli = 0;
-	char recvBuf[4];
-	SOCKET waiter;
+
+	int numClis = 0;
+	char addrs[4][16];
+	
 	while (!(GetAsyncKeyState(VK_ESCAPE) & 0x01)) {
-		waiter = accept(hostTCP, &newGuest, NULL);
-		if (ioctlsocket(waiter, FIONBIO, &mode) == SOCKET_ERROR) goto activityCheck;
-		FD_SET(waiter, &reads);
-	activityCheck:
-		if (reads.fd_count == 0)
-			continue;
+		struct sockaddr_in guestAddr;
+		SOCKET waiter = accept(hostTCP, NULL, NULL);
+		if (ioctlsocket(waiter, FIONBIO, &mode) != SOCKET_ERROR) FD_SET(waiter, &reads);
+		if (reads.fd_count == 0) continue;
+
 		select(0, &reads, NULL, NULL, &timeout);
 		for (int i = 0; i < reads.fd_count; i++) {
-			int bytes = recv(reads.fd_array[i], recvBuf, 4, 0);
-			if (bytes == 1 && recvBuf[0] == '0' && send(reads.fd_array[i], "0", 1, 0) != SOCKET_ERROR) {
+			char bufSendTCP[1];
+			char bufRecvTCP[1];
+			int numBytes = recv(reads.fd_array[i], bufRecvTCP, 1, 0);
+			int type = STRIP(bufRecvTCP[0], 00000000, 0);
+			int id = STRIP(bufRecvTCP[0], 00001110, 1);
+			if (numBytes == 0 || (numBytes == SOCKET_ERROR && WSAGetLastError() == 10054)) {
+				FD_CLR(reads.fd_array[i], &reads);
+				shutdown(reads.fd_array[i], SD_SEND);
+				closesocket(reads.fd_array[i]);
+			}
+			else if (type == 0) {
+				if (numClis == MAX_PLAYERS) {
+					bufSendTCP[0] = BUILD_TCP(0, 0);
+					send(reads.fd_array[i], bufSendTCP, 1, 0);
+					continue;
+				}
+				bufSendTCP[0] = BUILD_TCP(0, ++numClis);
+				/*
+				* for (int i = 0; ) {
+
+				}
+				*/
+				
+				send(reads.fd_array[i], bufSendTCP, 1, 0);
+				getpeername(reads.fd_array[i], &guestAddr, sizeof(struct sockaddr));
+				inet_ntop(AF_INET, &guestAddr.sin_addr, addrs[numClis], 16);
+				printf("%s joined\n%i players remain\n\n", addrs[numClis], numClis);
+			}
+			else if (type == 1) {
+				bufSendTCP[0] = BUILD_TCP(1, numClis - 1);
+				if (send(reads.fd_array[i], bufSendTCP, 1, 0) == SOCKET_ERROR) continue;
 				char ipBuf[16];
 				char* ip;
-				struct sockaddr_in addr;
-				getpeername(reads.fd_array[i], &addr, sizeof(struct sockaddr));
-				ip = inet_ntop(AF_INET, &addr.sin_addr, &ipBuf, 16);
-				clis[reads.fd_array[i]] = ip;
-				printf("connected sockets: %i\n", ++numCli);
+				getpeername(reads.fd_array[i], &guestAddr, sizeof(struct sockaddr));
+				ip = inet_ntop(AF_INET, &guestAddr.sin_addr, ipBuf, 16);
+				for (int i = 0; i < 4; i++) {
+					if (strcmp(ip, addrs[i]) == 0) {
+						printf("%s left\n%i players remain\n\n", addrs[i], numClis);
+						ZeroMemory(addrs[i], 16);
+						numClis--;
+						break;
+					}
+				}
 			}
-			else if (bytes == 1 && recvBuf[0] == '1' && send(reads.fd_array[i], "1", 1, 0) != SOCKET_ERROR) {
-				printf("connected sockets: %i\n", --numCli);
-			}
-			else if (bytes == 0)
-				FD_CLR(reads.fd_array[i], &reads);
-			// For people who send a TCP request then end program before recv
-			else if (bytes == SOCKET_ERROR && WSAGetLastError() == 10054) {	
-				
-			}
-		removeCli:
-			printf("Client %s disconnected\n", clis[reads.fd_array[i]]);
-			printf("There are now %i clients\n", --numCli);
-			clis[reads.fd_array[i]] = NULL;
-			FD_CLR(reads.fd_array[i], &reads);
-			shutdown(reads.fd_array[i], SD_SEND);
-			closesocket(reads.fd_array[i]);
 		}
 	}
 	for (int i = 0; i < reads.fd_count; i++) {
@@ -141,7 +128,6 @@ int main() {
 	}
 	FD_ZERO(&reads);
 end:
-	free(clis);
 	freeaddrinfo(addrTCP);
 	closesocket(hostTCP);
 	WSACleanup();
