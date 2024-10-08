50244797262b0a090b3b14beef784e19a2e259ab
LanceNoble <ltnlikespie@gmail.com>
2024-10-08 00:20:35 -0400

    server partially helps sync player positions

--- dumb/main.c
+++ dumb/main.c
@@ -20,61 +20,6 @@
 
 #define MAX_PLAYERS 3
 #define VACANT_PLAYER 0
-#define CREATE_PLAYERLIST(ptrList)		  \
-	struct player list[MAX_PLAYERS];	  \
-	for (int i = 0; i < MAX_PLAYERS; i++) \
-		list[i].id = VACANT_PLAYER;		  \
-	ptrList = list;
-#define ADD_PLAYER(ptrList, ptrNewPlayer) \
-	for (int i = 0; i < MAX_PLAYERS; i++) {					\
-		if (ptrList[i].id == VACANT_PLAYER) {				\
-			ptrList[i].sock = ptrNewPlayer->sock;			\
-			ptrList[i].addr = ptrNewPlayer->addr;			\
-			ptrList[i].id = i + 1;							\
-			ptrList[i].x = 0;								\
-			ptrList[i].y = 0;								\
-			ptrNewPlayer = &ptrList[i];						\
-			break;											\
-		}													\
-	}
-	
-#define ACCESS_PLAYER   (ptrList, id) ptrList[id - 1]
-#define REMOVE_PLAYER   (ptrList, id) ACCESS_PLAYER(ptrList, id).id = VACANT_PLAYER
-#define IS_PLAYER_TAKEN (ptrList, id) (int)(ACCESS_PLAYER(ptrList, id).id == VACANT_PLAYER)
-
-#define F8B  11111111
-#define BUILD_CONNECTION_STATUS(type, id1, id2, id3) (type << 6) | (id1 << 4) | (id2 << 2) | id3
-
-// Construct player position
-// b[0-1]   - player id
-// b[2]	    - sign
-//	      0 - negative
-//		  1 - positive
-// b[3-12]  - x coord of player
-// b[13]    - sign
-//        0 - negative
-//        1 - positive
-// b[14-23] - y coord of player
-#define BUILD_PLAYER_POSITION(id, spx, px, spy, py) (id << 22) | (spx << 21) | (px << 11) | (spy << 10) | py
-
-// Split a 24-bit player position into 3 bytes
-#define CHUNK_PLAYER_POSITION(F8B, pp, byte0, byte1, byte2) \
-	byte0 = (pp & (0b##F8B << 16)) >> 16;					\
-	byte1 = (pp & (0b##F8B << 8 )) >> 8 ;					\
-    byte2 =  pp &  0b##F8B 
-
-// Reattach a 24-bit player position that's been split into 3 bytes
-#define GLUE_PLAYER_POSITION(byte0, byte1, byte2) (byte0 << 16) | (byte1 << 8) | byte2
-
-#define STRIP_PLAYER_POSITION(pp, id, x, y)  \
-	id = (pp & (0b11          << 22)) >> 22; \
-	 x = (pp & (0b11111111111 << 11)) >> 11; \
-	 y =  pp &  0b11111111111			   ;                 
-
-#define STRIP_COORDINATE(coord, sign, num) \
-	sign = coord & 0b100000000000;		   \
-	num  = coord & 0b011111111111;		   
-
 
 // Represents a server-wide or player-specific msg:
 // - type: subject of msg describing an event
@@ -83,28 +28,36 @@
 //   - 2: player left the server (server-wide)
 // - target: id of player associated with event
 // - id1 + id2: ids of other players (only used with msg type 0)
-union out_tcp {
+union tcp_msg {
 	struct {
 		unsigned char type     : 2; 
 		unsigned char target   : 2;
-		unsigned char id1	   : 2;
-		unsigned char id2	   : 2;
+		unsigned char ids	   : 4;
 	};
-	char raw;
+	char raw[1];
+};
+
+union udp_msg {
+	struct {
+		unsigned int id : 2;
+		signed   int x  : 15;
+		signed   int y  : 15;
+	};
+	int whole;
+	char raw[4];
 };
 
 struct player {
-	SOCKET sock;
-	struct sockaddr addr;
-	int id;
-	int x;
-	int y;
+	SOCKET waiter;
+	unsigned int id;
+	signed int x;
+	signed int y;
+	struct sockaddr udpAddr;
 };
 
-SOCKET host(int type, int proto) {
+int host(int type, int proto, SOCKET* shost) {
 	int res;
 	u_long mode = 1;
-	SOCKET sock;
 	struct addrinfo* addr;
 	struct addrinfo hints;
 	ZeroMemory(&hints, sizeof(hints));
@@ -113,131 +66,178 @@ SOCKET host(int type, int proto) {
 	hints.ai_protocol = proto;
 	hints.ai_flags = AI_PASSIVE;
 	res = getaddrinfo(NULL, "3490", &hints, &addr);
-	if (res != 0) return res;
-	sock = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
-	if (sock == INVALID_SOCKET) { freeaddrinfo(addr); return WSAGetLastError(); }
-	if (ioctlsocket(sock, FIONBIO, &mode) == SOCKET_ERROR) { freeaddrinfo(addr); return WSAGetLastError(); }
-	if (bind(sock, addr->ai_addr, addr->ai_addrlen) == SOCKET_ERROR) { freeaddrinfo(addr); return WSAGetLastError(); }
-	return sock;
+	if (res != 0) 
+		return res;
+	*shost = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+	if (*shost == INVALID_SOCKET) {
+		freeaddrinfo(addr); 
+		return WSAGetLastError(); 
+	}
+	if (ioctlsocket(*shost, FIONBIO, &mode) == SOCKET_ERROR) {
+		freeaddrinfo(addr); 
+		return WSAGetLastError(); 
+	}
+	if (bind(*shost, addr->ai_addr, addr->ai_addrlen) == SOCKET_ERROR) {
+		freeaddrinfo(addr); 
+		return WSAGetLastError(); 
+	}
+	freeaddrinfo(addr);
+	return 0;
 }
 
 int main() {
+	int status;
+
 	// Initialize WSA
 	WSADATA wsaData;
-	int wsainitres = WSAStartup(MAKEWORD(2, 2), &wsaData);
-	if (wsainitres != 0) {
+	status = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (status != 0) {
 		printf("WSAStartup Fail");
-		return wsainitres;
+		return status;
 	}
 
 	// Initialize host sockets
-	SOCKET hostTCP = host(SOCK_STREAM, IPPROTO_TCP);
-	SOCKET hostUDP = host(SOCK_DGRAM, IPPROTO_UDP);
-	if (listen(hostTCP, SOMAXCONN) == SOCKET_ERROR) WSAGetLastError();
+	SOCKET hostTCP;
+	status = host(SOCK_STREAM, IPPROTO_TCP, &hostTCP);
+	if (status != 0)
+		return status;
+
+	if (listen(hostTCP, SOMAXCONN) == SOCKET_ERROR)
+		return WSAGetLastError();
+
+	SOCKET hostUDP; 
+	status = host(SOCK_DGRAM, IPPROTO_UDP, &hostUDP);
+	if (status != 0)
+		return status;
 
 	// Initialize player list
-	struct player* players;
-	CREATE_PLAYERLIST(players);
+	struct player players[MAX_PLAYERS];
+	ZeroMemory(players, sizeof(*players) * MAX_PLAYERS);
 
 	u_long mode = 1;
 	int numPlayers = 0;
-	int numBytes = 0;
-	char bufSendTCP[1];
-	char bufRecvTCP[1];
-	char bufRecvUDP[3];
-	union out_tcp msg;
 
 	while (!(GetAsyncKeyState(VK_END) & 0x01)) {
-
 		// Test 3: Player Validation
-		struct player* newPlayer;
-		newPlayer->sock = accept(hostTCP, &(newPlayer->addr), NULL);
-		if (numPlayers									 == MAX_PLAYERS    || 
-			newPlayer->sock								 == INVALID_SOCKET ||
-			ioctlsocket(newPlayer->sock, FIONBIO, &mode) == SOCKET_ERROR     ) {
+		if (numPlayers == MAX_PLAYERS)
+			goto checkup;
 
-			closesocket(newPlayer->sock);
+		SOCKET waiter = accept(hostTCP, NULL, NULL);
+		if (waiter == INVALID_SOCKET)
+			goto checkup;
+
+		if (ioctlsocket(waiter, FIONBIO, &mode) == SOCKET_ERROR) {
+			closesocket(waiter);
+			goto checkup;
+		}
+
+		struct player* next = NULL;
+		for (int i = 0; i < MAX_PLAYERS; i++) {
+			if (players[i].id == VACANT_PLAYER) {
+				next = &(players[i]);
+				next->waiter = waiter;
+				next->id = i + 1;
+				next->x = 0;
+				next->y = 0;
+				break;
+			}
+		}
+		if (next == NULL) {
+			closesocket(waiter);
 			goto checkup;
 		}
 		// Test 3 End
 
-		// Test 4: Add new player
-		union out_tcp msg;
-		msg.type = 0;
-		ADD_PLAYER(players, newPlayer);
-		msg.target = newPlayer->id;
-		// Populate the rest of the id array indices with the other players (if there are any)
+		union tcp_msg preData;
+		preData.type = 0;
+		preData.target = next->id;
+		preData.ids = 0;
 		for (int i = 0, j = 0; i < MAX_PLAYERS; i++) {
-			if (players[i].id == newPlayer->id) continue;
-			msg.raw |= (players[i].id << (4 + i * 2));
+			if (players[i].id == next->id) continue;
+			preData.ids |= (players[i].id << (2 * j));
 			j++;
 		}
+		send(next->waiter, preData.raw, sizeof(preData), 0);
 
-		// Notify the new player that they successfully joined
-		// And tell them who's already in the server
-		bufSendTCP[0] = 
-		send(newPlayer.sock, bufSendTCP, 1, 0);
-		
-		// Notify the other players that a new player joined
-		// And tell them who it is
-		bufSendTCP[0] = BUILD_CONNECTION_STATUS(1, ids[0], 0, 0);
+		union tcp_msg newData;
+		newData.type = 1;
+		newData.target = next->id;
+		newData.ids = 0;
 		for (int i = 0; i < MAX_PLAYERS; i++) 
-			if (players[i].id != newPlayer.id && players[i].id != 0) 
-				send(players[i].sock, bufSendTCP, 1, 0);
+			if (players[i].id != next->id && 
+				players[i].id != VACANT_PLAYER) 
+				send(players[i].waiter, newData.raw, sizeof(newData), 0);
 
 		numPlayers++;
 	checkup:
 		// Scan for any players who disconnected
 		for (int i = 0; i < MAX_PLAYERS; i++) {
-			if (players[i].id == 0) continue;
-			numBytes = recv(players[i].sock, bufRecvTCP, 1, 0);
+			if (players[i].id == VACANT_PLAYER) 
+				continue;
+
+			union tcp_msg oldData;
+			int numBytes = recv(players[i].waiter, oldData.raw, sizeof(oldData), 0);
+
 			// Secondary condition accounts for players who disconnect by force closing
-			if (numBytes == 0 || (numBytes == SOCKET_ERROR && WSAGetLastError() == 10054)) {
+			if (numBytes == 0 || 
+				(numBytes == SOCKET_ERROR && WSAGetLastError() == 10054)) {
+
+				oldData.type = 2;
+				oldData.target = players[i].id;
+				oldData.ids = 0;
+
+				closesocket(players[i].waiter);
+				ZeroMemory(&(players[i]), sizeof(players[i]));
+
+				for (int i = 0; i < MAX_PLAYERS; i++) 
+					if (players[i].id != VACANT_PLAYER) 
+						send(players[i].waiter, oldData.raw, sizeof(oldData), 0);
+
 				numPlayers--;
-				bufSendTCP[0] = BUILD_CONNECTION_STATUS(2, players[i].id, 0, 0);
-				closesocket(players[i].sock);
-				players[i].id = 0;
-				for (int i = 0; i < MAX_PLAYERS; i++) if (players[i].id != 0) send(players[i].sock, bufSendTCP, 1, 0);
 			}
 		}
 
-		char bufSendUDP[9] = { 0,0,0,0,0,0,0,0,0 };
-		int iBufSendUDP = 0;
-		for (int i = 0; i < MAX_PLAYERS; i++) {
-			if (players[i].id == 0) continue;
-			int sx, x, sy, y;
-			sx = players[i].x < 0;
-			x = abs(players[i].x);
-			sy = players[i].y < 0;
-			y = abs(players[i].y);
+		union udp_msg idealPos;
+		struct sockaddr udpAddr;
+		int udpAddrLen = sizeof(udpAddr);
+		int numBytes = recvfrom(hostUDP, idealPos.raw, sizeof(idealPos), 0, &udpAddr, &udpAddrLen);
 
-			int pp = BUILD_PLAYER_POSITION(players[i].id, sx, x, sy, y);
+		if (numBytes == SOCKET_ERROR)
+			continue;
+		
+		idealPos.whole = ntohl(idealPos.whole);
 
-			char ppChunks[3];
-			CHUNK_PLAYER_POSITION(F8B, pp, ppChunks[0], ppChunks[1], ppChunks[2]);
-			bufSendUDP[iBufSendUDP++] = ppChunks[0];
-			bufSendUDP[iBufSendUDP++] = ppChunks[1];
-			bufSendUDP[iBufSendUDP++] = ppChunks[2];
-		}
+		if (players[idealPos.id - 1].udpAddr.sa_family == 0)
+			players[idealPos.id - 1].udpAddr = udpAddr;
+
+		players[idealPos.id - 1].x = idealPos.x;
+		players[idealPos.id - 1].y = idealPos.y;
+
+		/*
+		idealPos.whole = htonl(idealPos.whole);
 		for (int i = 0; i < MAX_PLAYERS; i++)
-			if (players[i].id != 0)
-				sendto(hostUDP, bufSendUDP, 9, 0, &(players[i].addr), sizeof(struct sockaddr));
+			if (players[i].id != VACANT_PLAYER && 
+				players[i].udpAddr.sa_family != 0 &&
+				players[i].id != idealPos.id)
+				sendto(hostUDP, idealPos.raw, sizeof(idealPos), 0, &(players[i].udpAddr), sizeof(players[i].udpAddr));
+		*/
 
-		numBytes = recvfrom(hostUDP, bufRecvUDP, 3, 0, NULL, NULL);
-		if (numBytes != SOCKET_ERROR) {
-			int id, x, y;
-			STRIP_PLAYER_POSITION( (GLUE_PLAYER_POSITION(bufRecvUDP[0], bufRecvUDP[1], bufRecvUDP[2])) , id, x, y);
-
-			int sx, ax, sy, ay;
-			STRIP_COORDINATE(x, sx, ax);
-			STRIP_COORDINATE(y, sy, ay);
-			players[id - 1].x = ax;
-			players[id - 1].y = ay;
-			if (sx == 0) players[id - 1].x *= -1;
-			if (sy == 0) players[id - 1].y *= -1;
+		for (int i = 0; i < MAX_PLAYERS; i++) {
+			if (players[i].id == VACANT_PLAYER)
+				continue;
+			for (int j = 0; j < MAX_PLAYERS; j++) {
+				if (players[j].id == VACANT_PLAYER || players[j].id == players[i].id)
+					continue;
+				union udp_msg pos;
+				pos.id = players[j].id;
+				pos.x = players[j].x;
+				pos.y = players[j].y;
+				pos.whole = htonl(pos.whole);
+				sendto(hostUDP, pos.raw, sizeof(pos), 0, &(players[i].udpAddr), sizeof(players[i].udpAddr));
+			}
 		}
 	}
-	for (int i = 1; i < MAX_PLAYERS + 1; i++) if (players[i].id != 0) closesocket(players[i].sock);
+	for (int i = 0; i < MAX_PLAYERS; i++) if (players[i].id != VACANT_PLAYER) closesocket(players[i].waiter);
 	closesocket(hostTCP);
 	closesocket(hostUDP);
 	WSACleanup();
